135c5182080f4265294360428e24c15e75c45cea
Gabriel Sacramento <gabriel.ecomp@outlook.com>
2018-11-22 23:11:40 -0300

    novas regras

--- src/sintatico/Regras2.java
+++ src/sintatico/Regras2.java
@@ -1,14 +1,188 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sintatico;
+
+import java.util.List;
+import lexico.Token;
 
 /**
  *
  * @author Gabriel
  */
 public class Regras2 {
+    Regras regras = new Regras();
+    public int [] atrib (List <Token> lista){
+        
+        int [] retorno = new int [2];
+        int estado = 1;
+        
+        for (int i = 0; i < lista.size(); i++){
+            Token aux = lista.get(i);
+            if (estado == 1){
+                if (aux.getNome().equals("id")){
+                    estado = 2;
+                } else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 2){
+                if (aux.getNome().equals(".")){
+                    estado = 3;
+                } else if (aux.getNome().equals("(")){
+                    estado = 4;
+                } else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 3){
+                if (aux.getNome().equals("id")){
+                    estado = 6;
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                } else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 4){
+                if (regras.expression(lista)[0] == 1){
+                    estado = 5;
+                }  else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 5){
+                if (aux.getNome().equals(")")){
+                    estado = 6;
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                } else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 6){
+                if (aux.getNome().equals("(")){
+                    estado = 4;
+                } else {
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            }
+            
+        }
+        return retorno;
+    }
     
+    public int [] increment (List <Token> lista){
+        
+        int retorno [] = new int[2];
+        int estado = 1;
+        
+        for (int i = 0; i < lista.size(); i++){
+            Token aux = lista.get(i);
+            if (estado == 1){
+                if (aux.getTipo().equals("Identificador") || aux.getTipo().equals("Numero")){
+                    estado = 2;
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                }else if (aux.getNome().equals("++")|| aux.getNome().equals("--")){
+                    estado = 3;
+                }else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 2){
+                if (aux.getNome().equals("++")|| aux.getNome().equals("--")){
+                    estado = 4;
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                    return retorno;
+                } else {
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 3){
+                if (aux.getTipo().equals("Identificador") || aux.getTipo().equals("Numero")){
+                    estado = 4;
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                    return retorno;
+                }else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } 
+        }
+        
+        return retorno;
+    }
+    
+    public int [] atribuition (List <Token> lista){
+        
+        int retorno[] = new int [2];
+        int estado = 1;
+        
+        for (int i = 0; i < lista.size(); i++){
+            Token aux = lista.get(i);
+            if (estado == 1){
+                if (aux.getNome().equals("int") || aux.getNome().equals("float") || aux.getNome().equals("bool") || aux.getNome().equals("string")){
+                    estado = 2;
+                } else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 2){
+                if (aux.getTipo().equals("Identificador")){
+                    estado = 3;
+                }else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 3){
+                if (aux.getNome().equals("=")){
+                    estado = 4;
+                }else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if (estado == 4){
+                List<Token> sublista = lista.subList(i, (lista.size())); 
+                int numero = regras.expression(sublista)[0];
+                int numero1 = this.atrib(sublista)[0];
+                int numero2 = this.increment(sublista)[0];
+                
+                if (aux.getTipo().equals("Cadeia de caracteres") || aux.getNome().equals("true") || aux.getNome().equals("false") 
+                       || numero == 1 || numero1 == 1 || numero2 == 1){
+                    estado = 5;
+                }else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            } else if(estado == 5){
+                if (aux.getNome().equals(";")){
+                    retorno[0] = 1;
+                    retorno[1] = i;
+                    return retorno;
+                } else {
+                    retorno[0] = 0;
+                    retorno[1] = i;
+                    return retorno;
+                }
+            }
+        }
+        return retorno;
+    }
+    
+        
 }
